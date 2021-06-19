ecafe5aa345792213113f8bbd8344cb1f59a59bd
LIUC2002 <83438334+LIUC2002@users.noreply.github.com>
2021-06-19 16:19:30 +0800

    Add files via upload

---
+++ Classes/Resources/RoleSelectScene/backhomeItem.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="backhomeItem" tilewidth="10" tileheight="10" tilecount="270" columns="18">
+ <image source="backhomeItem.png" width="180" height="150"/>
+</tileset>
